b7bc14a587ccc93373379bb8301aa38495750756
tasnim.raslan <tasnim.raslan@fing.edu.uy>
2023-09-02 21:29:15 -0300

    fix schema change foreign key names

--- first_schema.sql
+++ first_schema.sql
@@ -1,5 +1,7 @@
 CREATE DATABASE calendario;
 
+USE calendario;
+
 CREATE TABLE users (
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
     username VARCHAR(50) NOT NULL,
@@ -12,19 +14,19 @@ CREATE TABLE labels (
     user_id INT UNSIGNED NOT NULL,
     name VARCHAR(30),
     color VARCHAR(6),
-    CONSTRAINT labels_pk PRIMARY KEY (user_id, name),
-    CONSTRAINT user_id_fk FOREIGN KEY (user_id) REFERENCES users(id)
+    CONSTRAINT labels_pk PRIMARY KEY (name, user_id),
+    CONSTRAINT labels_user_id_fk FOREIGN KEY (user_id) REFERENCES users(id)
 );
 
 CREATE TABLE events (
     user_id INT UNSIGNED NOT NULL,
     name VARCHAR(50),
     description VARCHAR(255),
-    start_date TIMESTAMP,
-    end_date TIMESTAMP,
-    event_period INT, -- expressed in minutos
-    CONSTRAINT events_pk PRIMARY KEY (user_id, name),
-    CONSTRAINT user_id_fk FOREIGN KEY (user_id) REFERENCES users(id)
+    start_date TIMESTAMP NULL ,
+    end_date TIMESTAMP NULL,
+    event_period INT, -- expressed in minutes
+    CONSTRAINT events_pk PRIMARY KEY (name, user_id),
+    CONSTRAINT events_user_id_fk FOREIGN KEY (user_id) REFERENCES users(id)
 );
 
 CREATE TABLE labels_events (
@@ -33,9 +35,9 @@ CREATE TABLE labels_events (
     label_name VARCHAR(50),
     event_name VARCHAR(50),
     CONSTRAINT labels_events_pk PRIMARY KEY (id),
-    CONSTRAINT user_id_fk FOREIGN KEY (user_id) REFERENCES users(id),
-    CONSTRAINT label_name_fk FOREIGN KEY (label_name) REFERENCES labels(name),
-    CONSTRAINT event_name_fk FOREIGN KEY (event_name) REFERENCES events(name)
+    CONSTRAINT labels_events_user_id_fk FOREIGN KEY (user_id) REFERENCES users(id),
+    CONSTRAINT labels_events_label_name_fk FOREIGN KEY (label_name) REFERENCES labels(name),
+    CONSTRAINT labels_events_event_name_fk FOREIGN KEY (event_name) REFERENCES events(name)
 )
 
 
