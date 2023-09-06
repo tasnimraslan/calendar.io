b76a092a382becb44de7d24b2a3927ed24c6ed28
tasnim.raslan <tasnim.raslan@fing.edu.uy>
2023-09-06 00:30:35 -0300

    add main spring class

---
+++ api/src/Calendario.java
@@ -0,0 +1,11 @@
+package api.src;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Calendario {
+    public static void main(String[] args) {
+        SpringApplication.run(Calendario.class, args);
+    }
+}
