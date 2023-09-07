978966b37dc32b30d9e3a87e112d2e782ba1536e
tasnim.raslan <tasnim.raslan@fing.edu.uy>
2023-09-06 21:57:27 -0300

    add label entity and repository

---
+++ model/src/main/java/Label.java
@@ -0,0 +1,15 @@
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Entity
+@Data
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Label {
+    private @Id Long userId;
+    private @Id Long name;
+    private String color;
+}
